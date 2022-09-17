da5349140794c805a6de5588e74ad7e40fcc7888
NADaza122 <an.nidaza.122n@gmail.com>
2022-09-17 17:02:35 -0500

    commit9

--- assets/js/punto1.js
+++ assets/js/punto1.js
@@ -1,25 +1,29 @@
 //EJERCICIO PALABRA//
+//FUNCION PARA EL INPUT DE LAS PALABRAD DIGITADAS//
+  function input(){
+    const palabra = new palabra(document.getElementById("palabra").value);
+};
 
 /*LONGITUD*/
- function Longitud(){
-    let palabra=prompt("Digite aqui la palabra para hallar su Longitud:")
+  function Longitud(){
+    let palabra=document.getElementById("palabra").value;
     swal('Longitud','La Longitud de su palabra es: ' + palabra.length, 'success');
 }
 
 /*MAYUSCULAS*/
-function Mayusculas(){
-let palabra=prompt("Digite aqui la palabra para pasar de Minusculas a Mayusculas:")
+  function Mayusculas(){
+    let palabra=document.getElementById("palabra").value;
     swal('Mayusculas','Su palabra en Mayusculas es: ' + palabra.toUpperCase(),'success');
 }
 
 /*MINUSCULAS*/
-function Minusculas(){
-let palabra=prompt("Digite aqui la palabra para pasar de Mayusculas a Minusculas:")
+  function Minusculas(){
+    let palabra=document.getElementById("palabra").value;
     swal('Minusculas','Su palabra en Minisculas es: ' + palabra.toLowerCase(), 'success');
 }
 
 /*PRIMERA LETRA*/
-function PrimeraL(){
-let palabra=prompt("Digite aqui la palabra para hallar la Primera letra:")
+  function PrimeraL(){
+    let palabra=document.getElementById("palabra").value;
     swal('Primera Letra','La Primera letra de su palabra es: ' + palabra.charAt(), 'success');
 }
